5ca30058f44a8784662bd9b229bf500ab2a8031a
Hafizh Herdi <Herdi@Herdis-MacBook-Pro.local>
2018-04-25 00:12:42 +0700

    add commentary

--- app/src/main/java/co/twoh/kotlin/twohkotlin/MainActivity.kt
+++ app/src/main/java/co/twoh/kotlin/twohkotlin/MainActivity.kt
@@ -23,13 +23,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * Find view menggunakan as Button
+         */
         button1 = findViewById(R.id.bt_1) as Button
+
+        /**
+         * Find view secara langsung
+         */
         button2 = findViewById(R.id.bt_2)
 
+        /**
+         * setOnClickListener kemungkinan variable null
+         */
         button1?.setOnClickListener {
             Toast.makeText(this, "Clicked "+button1?.text, Toast.LENGTH_LONG).show()
         }
 
+        /**
+         * setOnClickListener variable tidak mungkin null
+         */
         button2.setOnClickListener{
             Toast.makeText(this, "Clicked "+button2.text, Toast.LENGTH_LONG).show()
         }
